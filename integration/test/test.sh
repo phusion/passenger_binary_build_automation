d204776e2bbd7a5fc18f0a6589526debf9bd582c
Hongli Lai (Phusion) <hongli@phusion.nl>
2016-11-30 17:00:12 +0100

    Fix Jenkins test script compatibility with Passenger Enterprise

--- integration/test/test.sh
+++ integration/test/test.sh
@@ -6,6 +6,7 @@
 # Required environment variables:
 #
 #   WORKSPACE
+#   ENTERPRISE (true or false)
 #
 # Optional environment variables:
 #
@@ -22,9 +23,15 @@ cd "$SELFDIR/../.."
 source "./shared/lib/library.sh"
 
 require_envvar WORKSPACE "$WORKSPACE"
+require_envvar ENTERPRISE "$ENTERPRISE"
 
 PASSENGER_ROOT="${PASSENGER_ROOT:-$WORKSPACE}"
 CONCURRENCY=${CONCURRENCY:-2}
+
+if [[ ! -e ~/passenger-enterprise-license ]]; then
+	echo "ERROR: ~/passenger-enterprise-license required."
+	exit 1
+fi
 
 # Sleep for a random amount of time in order to work around Docker/AUFS bugs
 # that may be triggered if multiple containers are shut down at the same time.
@@ -64,7 +71,8 @@ run ./linux/test \
 	-p "$PASSENGER_ROOT" \
 	-i "$WORKSPACE/output/x86" \
 	-I "$WORKSPACE/output/x86" \
-	-a x86
+	-a x86 \
+	-L ~/passenger-enterprise-license
 
 echo
 echo "---------- Testing x86_64 binaries ----------"
@@ -76,4 +84,5 @@ run ./linux/test \
 	-p "$PASSENGER_ROOT" \
 	-i "$WORKSPACE/output/x86_64" \
 	-I "$WORKSPACE/output/x86_64" \
-	-a x86_64
+	-a x86_64 \
+	-L ~/passenger-enterprise-license
