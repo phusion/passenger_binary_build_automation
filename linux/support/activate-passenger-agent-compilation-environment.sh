be8b48d5a2c15a42d86ba988b69d5e03206bfa5e
Hongli Lai (Phusion) <hongli@phusion.nl>
2016-11-30 14:30:18 +0100

    Fix setting compilation flags on Linux

--- linux/support/activate-passenger-agent-compilation-environment.sh
+++ linux/support/activate-passenger-agent-compilation-environment.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+set -o pipefail
 source /hbb_exe_gc_hardened/activate
 # Remove -O2 because the Passenger build system already
 # sets optimization flags.
 export EXTRA_CFLAGS=`echo "$CFLAGS" | sed 's/-O2//'`
-export EXTRA_CXXFLAGS=`eco "$CXXFLAGS" | sed 's/-O2//'`
+export EXTRA_CXXFLAGS=`echo "$CXXFLAGS" | sed 's/-O2//'`
 export EXTRA_LDFLAGS="$LDFLAGS"
 exec "$@"
