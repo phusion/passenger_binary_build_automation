347ebfba1c9da5a75b4bb015919647d78b628084
Hongli Lai (Phusion) <hongli@phusion.nl>
2017-07-17 15:31:02 +0200

    linux/test: activate RVM when running copy-passenger-source-dir.sh
    This fixes the non-Git copying strategy.

--- linux/support/test-script-docker-entrypoint.sh
+++ linux/support/test-script-docker-entrypoint.sh
@@ -12,7 +12,9 @@ export PASSENGER_ROOT=/tmp/passenger
 export PACKAGED_ARTEFACTS_DIR=/packaged
 export UNPACKAGED_ARTEFACTS_DIR=/unpackaged
 
-run setuser builder /system/shared/build/copy-passenger-source-dir.sh /passenger /tmp/passenger
+run setuser builder \
+	/usr/local/rvm/bin/rvm-exec ruby-$LAST_RUBY_VERSION \
+	/system/shared/build/copy-passenger-source-dir.sh /passenger /tmp/passenger
 run_exec setuser builder \
 	/usr/local/rvm/bin/rvm-exec ruby-$LAST_RUBY_VERSION \
 	rspec --tty -c -f d /system/shared/test/integration_test.rb "$@"
