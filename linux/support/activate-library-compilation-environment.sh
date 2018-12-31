62336a98ba8a93c5aca30d6446a411a5099be7a0
Camden Narzt <c.narzt@me.com>
2018-12-31 14:45:06 -0700

    third attempt to avoid linking libfreebl3

--- linux/support/activate-library-compilation-environment.sh
+++ linux/support/activate-library-compilation-environment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 source /hbb_shlib/activate
-export CFLAGS="$SHLIB_CFLAGS"
-export LDFLAGS="$SHLIB_LDFLAGS --as-needed"
-export EXTRA_CFLAGS="$SHLIB_CFLAGS"
-export EXTRA_CXXFLAGS="$SHLIB_CXXFLAGS"
-export EXTRA_LDFLAGS="$SHLIB_LDFLAGS --as-needed"
+export CFLAGS="-Wl,--as-needed $SHLIB_CFLAGS"
+export LDFLAGS="--as-needed $SHLIB_LDFLAGS"
+export EXTRA_CFLAGS="-Wl,--as-needed $SHLIB_CFLAGS"
+export EXTRA_CXXFLAGS="-Wl,--as-needed $SHLIB_CXXFLAGS"
+export EXTRA_LDFLAGS="--as-needed $SHLIB_LDFLAGS"
 exec "$@"
