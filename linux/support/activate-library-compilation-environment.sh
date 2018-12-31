bdf3997fd1742e8428751af1436ac496efef43f2
Camden Narzt <c.narzt@me.com>
2018-12-31 16:14:56 -0700

    Revert "experiment to ignore unused libs"
    This reverts commit a484b47087d8e3c65820b0942f7bcd44fdeb6d76.

--- linux/support/activate-library-compilation-environment.sh
+++ linux/support/activate-library-compilation-environment.sh
@@ -3,8 +3,6 @@ set -e
 source /hbb_shlib/activate
 export CFLAGS="$SHLIB_CFLAGS"
 export LDFLAGS="$SHLIB_LDFLAGS"
-export DLDFLAGS="--as-needed"
-export LOCAL_LIBS="--as-needed"
 export EXTRA_CFLAGS="$SHLIB_CFLAGS"
 export EXTRA_CXXFLAGS="$SHLIB_CXXFLAGS"
 export EXTRA_LDFLAGS="$SHLIB_LDFLAGS"
