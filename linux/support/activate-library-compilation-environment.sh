6b3cb3e099cf28dd33a0174474e8e8c220ba8123
Camden Narzt <c.narzt@me.com>
2018-12-31 15:47:24 -0700

    try setting dldflags and local_libs

--- linux/support/activate-library-compilation-environment.sh
+++ linux/support/activate-library-compilation-environment.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 source /hbb_shlib/activate
-export CFLAGS="-Wl,--as-needed $SHLIB_CFLAGS"
-export LDFLAGS="--as-needed $SHLIB_LDFLAGS"
-export EXTRA_CFLAGS="-Wl,--as-needed $SHLIB_CFLAGS"
-export EXTRA_CXXFLAGS="-Wl,--as-needed $SHLIB_CXXFLAGS"
-export EXTRA_LDFLAGS="--as-needed $SHLIB_LDFLAGS"
+export CFLAGS="$SHLIB_CFLAGS"
+export LDFLAGS="$SHLIB_LDFLAGS"
+export DLDFLAGS="--as-needed"
+export LOCAL_LIBS="--as-needed"
+export EXTRA_CFLAGS="$SHLIB_CFLAGS"
+export EXTRA_CXXFLAGS="$SHLIB_CXXFLAGS"
+export EXTRA_LDFLAGS="$SHLIB_LDFLAGS"
 exec "$@"
