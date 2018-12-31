038033506d6c9efa13cae6ddaa2a1848460cec64
Camden Narzt <c.narzt@me.com>
2018-12-31 14:33:55 -0700

    second attempt to ignore libfreebl3
    also try to print some debug info

--- linux/support/activate-library-compilation-environment.sh
+++ linux/support/activate-library-compilation-environment.sh
@@ -2,8 +2,8 @@
 set -e
 source /hbb_shlib/activate
 export CFLAGS="$SHLIB_CFLAGS"
-export LDFLAGS="$SHLIB_LDFLAGS -zignore"
+export LDFLAGS="$SHLIB_LDFLAGS --as-needed"
 export EXTRA_CFLAGS="$SHLIB_CFLAGS"
 export EXTRA_CXXFLAGS="$SHLIB_CXXFLAGS"
-export EXTRA_LDFLAGS="$SHLIB_LDFLAGS -zignore"
+export EXTRA_LDFLAGS="$SHLIB_LDFLAGS --as-needed"
 exec "$@"
