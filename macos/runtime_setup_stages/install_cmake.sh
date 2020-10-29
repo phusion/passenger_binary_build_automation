cfa5481f743aca8103660733e3f6e5306c469d33
Camden Narzt <c.narzt@me.com>
2020-10-29 12:39:03 -0600

    log issue with c++ compiler

--- macos/runtime_setup_stages/install_cmake.sh
+++ macos/runtime_setup_stages/install_cmake.sh
@@ -14,7 +14,7 @@ download_and_extract cmake-$CMAKE_VERSION.tar.gz \
 	cmake-$CMAKE_VERSION \
 	https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.tar.gz
 run rm -f "$WORKDIR/cmake-$CMAKE_VERSION.tar.gz"
-run ./bootstrap --prefix="$OUTPUT_DIR" --parallel=$CONCURRENCY
+run ./bootstrap --prefix="$OUTPUT_DIR" --parallel=$CONCURRENCY || (cat Bootstrap.cmk/cmake_bootstrap.log && false)
 run make -j$CONCURRENCY
 run make install
 run strip "$OUTPUT_DIR/bin/cmake"
