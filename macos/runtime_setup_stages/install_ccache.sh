6260d98dcd5f1f174e98702ce19acd4110735aba
Camden Narzt <c.narzt@me.com>
2020-11-01 11:16:25 -0700

    build ccache with clang++ not clang

--- macos/runtime_setup_stages/install_ccache.sh
+++ macos/runtime_setup_stages/install_ccache.sh
@@ -4,6 +4,9 @@ ROOTDIR=`dirname "$0"`
 ROOTDIR=`cd "$ROOTDIR/../.." && pwd`
 source "$ROOTDIR/shared/lib/library.sh"
 
+# the macos/support/bin/c++ script incorrectly uses cc instead of c++ here
+export CXX=/usr/bin/c++
+export CXXFLAGS="-std=c++11"
 CCACHE_VERSION=$(cat "$ROOTDIR/shared/definitions/ccache_version")
 
 header "Installing ccache $CCACHE_VERSION"
