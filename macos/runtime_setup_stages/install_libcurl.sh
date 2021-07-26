a808a46e7aaa5c7d0ead6d7bb4d9f1b3ff6ffc81
Camden Narzt <c.narzt@me.com>
2021-07-26 12:40:20 -0600

    make curl use openssl instead of secure transport on later macOS versions

--- macos/runtime_setup_stages/install_libcurl.sh
+++ macos/runtime_setup_stages/install_libcurl.sh
@@ -8,17 +8,24 @@ CURL_VERSION=$(cat "$ROOTDIR/shared/definitions/curl_version")
 
 header "Installing libcurl $CURL_VERSION"
 download_and_extract curl-$CURL_VERSION.tar.gz \
-	curl-$CURL_VERSION \
-	https://curl.haxx.se/download/curl-$CURL_VERSION.tar.gz
+		     curl-$CURL_VERSION \
+		     https://curl.haxx.se/download/curl-$CURL_VERSION.tar.gz
+
+if [ "$(curl-config --configure | fgrep libressl)" ]; then
+	TLS_LIB="--with-openssl --with-ca-bundle=/etc/ssl/cert.pem --with-ca-path=/etc/ssl/certs/"
+else
+	TLS_LIB="--with-secure-transport --without-ca-bundle --without-ca-path"
+fi
+
 run rm -f "$WORKDIR/curl-$CURL_VERSION.tar.gz"
 run ./configure --prefix="$OUTPUT_DIR" \
-	--disable-shared --disable-debug --enable-optimize --disable-werror \
-	--disable-curldebug --enable-symbol-hiding --disable-ares --disable-manual --disable-ldap --disable-ldaps \
-	--disable-rtsp --disable-dict --disable-ftp --disable-ftps --disable-gopher --disable-imap \
-	--disable-imaps --disable-pop3 --disable-pop3s --without-librtmp --disable-smtp --disable-smtps \
-	--disable-telnet --disable-tftp --disable-smb --disable-versioned-symbols \
-	--without-libmetalink --without-libidn2 --without-libssh2 --without-libmetalink --without-nghttp2 \
-	--with-secure-transport --without-ca-bundle --without-ca-path
+    --disable-shared --disable-debug --enable-optimize --disable-werror \
+    --disable-curldebug --enable-symbol-hiding --disable-ares --disable-manual --disable-ldap --disable-ldaps \
+    --disable-rtsp --disable-dict --disable-ftp --disable-ftps --disable-gopher --disable-imap \
+    --disable-imaps --disable-pop3 --disable-pop3s --without-librtmp --disable-smtp --disable-smtps \
+    --disable-telnet --disable-tftp --disable-smb --disable-versioned-symbols \
+    --without-libmetalink --without-libidn2 --without-libssh2 --without-nghttp2 \
+    ${TLS_LIB}
 run make -j$CONCURRENCY
 run make install-strip
 run rm -f "$OUTPUT_DIR/bin/curl"
