9788e58bd32c1f46d5acc3f91c198c6f29be43d5
Camden Narzt <c.narzt@me.com>
2021-09-27 12:12:23 -0600

    fix rake version in bianry build image
    and make building the image more reliable

--- linux/docker_image/build_stages/install_rvm.sh
+++ linux/docker_image/build_stages/install_rvm.sh
@@ -4,7 +4,40 @@ source /pbba_build/shared/lib/library.sh
 source /hbb/activate
 
 header "Installing RVM"
-run gpg --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+KEYSERVERS=(
+	hkp://keyserver.pgp.com
+	hkp://keys.gnupg.net
+	ha.pool.sks-keyservers.net
+	hkp://p80.pool.sks-keyservers.net:80
+	hkp://ipv4.pool.sks-keyservers.net
+	keyserver.ubuntu.com
+	hkp://keyserver.ubuntu.com:80
+	hkp://pgp.mit.edu
+	pgp.mit.edu
+	-end-
+)
+
+KEYS=(
+	409B6B1796C275462A1703113804BB82D39DC0E3
+	7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+)
+
+# We've had too many problems with keyservers. No matter which one we pick,
+# it will fail some of the time for some people. So just try a whole bunch
+# of them.
+for KEY in "${KEYS[@]}"; do
+	for KEYSERVER in "${KEYSERVERS[@]}"; do
+		if [[ "$KEYSERVER" = -end- ]]; then
+			echo 'ERROR: exhausted list of keyservers' >&2
+			exit 1
+		else
+			echo "+ gpg --keyserver $KEYSERVER --recv-keys ${KEY}"
+			gpg --keyserver "$KEYSERVER" --recv-keys "${KEY}" && break || echo 'Trying another keyserver...'
+		fi
+	done
+done
+
 run curl -L -o /tmp/install-rvm.sh https://get.rvm.io
 run bash /tmp/install-rvm.sh stable
 run /usr/local/rvm/bin/rvm autolibs disable
